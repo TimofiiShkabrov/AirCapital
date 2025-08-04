7ed045f2ca2e9118981d8cd5e101d001a6295256
Tymofii Shkabrov <timofii.shkabrov@gmail.com>
2025-08-04 15:44:11 +0200

    Initial Commit

---
+++ AirCapital/AirCapitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirCapitalApp.swift
+//  AirCapital
+//
+//  Created by Тимофей Шкабров on 04.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AirCapitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
