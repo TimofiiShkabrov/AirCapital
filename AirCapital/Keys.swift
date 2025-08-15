79bd9794a3f87da8214eb26d0b62ebb2a512c24b
Tymofii Shkabrov <timofii.shkabrov@gmail.com>
2025-08-15 09:38:22 +0200

    Version: 0.8 - Updated key configuration file.

--- AirCapital/Keys.swift
+++ AirCapital/Keys.swift
@@ -8,22 +8,22 @@
 import Foundation
 
 // MARK: - Binance APIKey
-let apiKeyBinance = "umKoPnwnfkJzvY0bB1iUjxYTP10waae6pfm1z6OafoSdLPVcSxJFOhyRvXbXmgQq"
-let secretKeyBinance = "VInBLTR6hB1mBF1yMBhFKFpECarBc3he9rObhlQfrZLJg6Rt8DCOSKO5vVC0i76B"
+let apiKeyBinance = ""
+let secretKeyBinance = ""
 
 // MARK: - Bybit APIKey
-let apiKeyBybit = "zEhBMcmnA2kVL6OyAf"
-let secretKeyBybit = "mOLfb7kVrKmcAkMow8y2OnLCRp0FTEK8uLu5"
+let apiKeyBybit = ""
+let secretKeyBybit = ""
 
 // MARK: - Bingx APIKey
-let apiKeyBingx = "kWgP65n8qMUIRKWM1dpXUFjxogdcjpKuE4VZ2Y5BLN6O009AcRSqJSM8D0oC8WLShUEp8tsmSnCd5oGr6AMA"
-let secretKeyBingx = "cz7On5MudI4z3X9lbP39m19QpaWnrL6tGEDQkRB0w0O93crPSv5x6PAhiES8BRuaVShTIhbJP4sxnVcldctGg"
+let apiKeyBingx = ""
+let secretKeyBingx = ""
 
 // MARK: - Gateio APIKey
-let apiKeyGateio = "2db5b51329c7f07c4b3f2ec6ee10824a"
-let secretKeyGateio = "df637fb440bf67f3b8f562e3622120802af9d30636322e3691f9a73296d3422b"
+let apiKeyGateio = ""
+let secretKeyGateio = ""
 
 // MARK: - OKX APIKey
-let apiKeyOkx = "8401810c-8f18-4c4d-90c7-c71783c6fffa"
-let secretKeyOkx = "469680E872922BE009C551D4D9B62B9B"
-let passphraseKeyOkx = "QWE456!qaz"
+let apiKeyOkx = ""
+let secretKeyOkx = ""
+let passphraseKeyOkx = ""
