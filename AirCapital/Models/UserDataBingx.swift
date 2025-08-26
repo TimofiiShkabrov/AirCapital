956a01dff784e1a3a2e65f03a7094af4c25c931f
Tymofii Shkabrov <timofii.shkabrov@gmail.com>
2025-08-26 21:13:37 +0200

    Version: 0.7 - Add UserDataOkx; - Updated network layer; - Updated signature manager.

---
+++ AirCapital/Models/UserDataBingx.swift
@@ -0,0 +1,44 @@
+//
+//  UserDataBingx.swift
+//  AirCapital
+//
+//  Created by Тимофей Шкабров on 12.08.2025.
+//
+
+import Foundation
+
+// MARK: - UserDataBingxSpot
+struct UserDataBingxSpot: Codable {
+    let code: Int
+    let msg, debugMsg: String?
+    let data: DataClass?
+}
+
+struct DataClass: Codable {
+    let balances: [Balance]
+}
+
+struct Balance: Codable {
+    let asset, free, locked: String
+}
+
+
+// MARK: - UserDataBingxFutures
+struct UserDataBingxFutures: Codable {
+    let code: Int64
+    let msg: String
+    let data: [BingxFuturesData]
+}
+
+struct BingxFuturesData: Codable {
+    let userId: String
+    let asset: String
+    let balance: String
+    let equity: String
+    let unrealizedProfit: String
+    let realisedProfit: String
+    let availableMargin: String
+    let usedMargin: String
+    let freezedMargin: String
+    let shortUid: String
+}
